25fb632bdde01936815100a224ec8f9a896b0ecf
Soo <Leviathan1995@outlook.com>
2015-05-26 23:36:52 +0800

    update

--- Lmachine.cpp
+++ Lmachine.cpp
@@ -1,4 +1,83 @@
 #include <iostream>
+#include <boost/regex.hpp>
+#include <fstream>
+#include "Lmachine.h"
 using namespace std;
-#define IADDR_SIZE 1024
-#define DADDR_SIZE 1024
+//初始化
+void Lmachine::Init()
+{
+	/*
+		打开的指定的汇编代码文件
+	*/
+	Code.open("code.txt");
+	/*
+		初始化
+	*/
+	PC = 0;
+	Data[0] = Data_SIZE - 1;
+	for (int i = 1; i < Data_SIZE; i++) //清空数据区
+		Data[i] = 0;
+	for (int i = 0; i < Operand_SIZE; i++)//初始化只读指令存储区
+	{
+		OperandMem[i].op = OpHLT;
+		OperandMem[i].data1 = 0;
+		OperandMem[i].data2 = 0;
+		OperandMem[i].data3 = 0;
+	}
+	for (int i = 0; i = RegisterNum; i++)//清空寄存器
+		Register[i] = 0;
+}
+//将Code一行一行读取到LmachineToken队列中
+void Lmachine::ReadLine()
+{
+	string Line;
+	while (getline(Code, Line)) //每行读取汇编代码
+	{
+		if (boost::regex_search(Line, what, Regex))
+		{
+			while (what[0].length() != 0)
+			{
+				if (what[2].matched == true) //number
+				{
+					string tokentext(what[2].first, what[2].second);
+					int tokennumber = atoi(tokentext.c_str());
+					Token token(tokennumber);
+					LmachineToken.push(token);
+					if ((Line.length() - tokentext.length() - 1) == 0)
+						break;
+					Line = Line.substr((Line.length() - tokentext.length() - 1));
+				}
+				else if (what[3].matched == true) //ID
+				{
+					string tokentext(what[3].first, what[3].second);
+					Token token(tokentext);
+					LmachineToken.push(token);
+					if ((Line.length() - tokentext.length() - 1) == 0)
+						break;
+					Line = Line.substr((Line.length() - tokentext.length() - 1));
+				}
+				if (boost::regex_search(Line, what, Regex))
+				{
+					continue;
+				}
+			}
+		}
+	}
+}
+int Lmachine::OpClass(int c)
+{
+	if (c <= OpRRMODE)
+		return OperandRR;
+	else if (c <= OpRMMODE)
+		return OperandRM;
+	else
+		return OperandRA;
+}
+//将指令加载到只读指令存储区
+void Lmachine::Load()
+{
+	while (!LmachineToken.empty())
+	{
+		OperandMem[PC].op = Token2Op(LmachineToken.front());
+	}
+}
--- Lmachine.h
+++ Lmachine.h
@@ -1,8 +1,21 @@
 #pragma once
 #ifndef _LMACHINE_H
 #define _LMACHINE_H
+#include <iostream>
+#include <string>
+#include <queue>
+#include <fstream>
+using namespace std;
+#define Operand_SIZE 1024 //只读指令存储器大小 
+#define Data_SIZE 1024 //数据区大小
+#define RegisterNum 8   //寄存器数目
+#define PC_Register 7   //程序计数器的下标
+
+boost::regex Regex("(\s)|([0-9]+)|([A-Z_a-z]*[A-Z_a-z0-9]+)|(;)"); //正则表达式
+boost::smatch what;
 enum Operand
 {
+	//RR
 	OpHLT,//CPU暂停指令 格式：HLT
 	OpIN,//从端口读取数据
 	OpOUT,//向端口写数据
@@ -10,24 +23,114 @@ enum Operand
 	OpSUB,
 	OpMUL,
 	OpDIV,
-	OpRADDRMODE,//寄存器寻址
+	OpRRMODE,//寄存器寻址
+	//RM 
 	OpLOAD,//读取
 	OpSTORE,//存储
-	OpMADDRMODE,//内存中寻址
-
+	OpRMMODE,//内存中寻址
+	//RA
+	OpLDA,
+	OpLDC,
 	OpJLT,
 	OpJLE,
 	OpJGT,
 	OpJGE,
 	OpJEQ,
 	OpJNE,
+	OpRAMODE,
 
+};
+enum OpCode //三种不同类型的opcode
+{
+	OperandRR,
+	OperandRM,
+	OperandRA,
+};
+enum Result
+{
+	OK,		//正确执行
+	HALT,	//停止执行
+	IMEM_ERROR,	//IMEM错误
+	DMEM_ERROR,	//DMEM错误
+	ZERO_ERROR，//零除错误
 };
 struct Command
 {
 	Operand op;
-	int data1;
-	int data2;
-	int data3;
+	int data1;	//r
+	int data2;	//s
+	int data3;	//t
 };
+string OpCodeTable[] =
+{
+	//RR
+	"HLT",
+	"IN",
+	"OUT",
+	"ADD",
+	"SUB",
+	"MUL",
+	"DIV",
+	"????"
+	//RM
+	"LD",
+	"ST",
+	"????",
+	//RA
+	"LDA",
+	"LDC",
+	"JLT",
+	"JLE",
+	"JGT",
+	"JGE",
+	"JEQ",
+	"JNE",
+	"????",
+};
+string ResultTable[] =
+{
+	"OK",
+	"HLT"
+	"INSTRUCTION MEMORY FAULT",
+	"DATA MEMORY FAULT",
+	"DIVISION BY 0"
+};
+class Token
+{
+public:
+	Token();
+	Token(int number);
+	Token(string ID);
+	string ID;
+	int Number;
+	bool IsID();
+	bool IsNumber();
+	string GetID();
+	string GetNumber();
+};
+class Lmachine
+{
+public:
+	/*
+	数据成员
+	*/
+	Command OperandMem[Operand_SIZE];//只读指令存储区
+	int Data[Data_SIZE];//数据存储区
+	int Register[RegisterNum];//寄存器
+	queue<Token> LmachineToken; //汇编代码字符流
+	fstream Code;//要加载的汇编文件
+	int IADDR_Pointer;//指向只读指令存储区的指针
+	int DADDR_Pointer;//指向数据存储区的指针
+	/*
+	成员函数
+	*/
+	int PC;//程序指令计数
+	void Init();//初始化
+	void ReadLine();//将FILE类型Code名字读取到LmachineToken中
+	void Load();//加载汇编代码到只读指令存储区
+	string Error();//输出错误信息
+	Operand Token2Op(Token token);//将Token转换为枚举类型的Operand
+	int OpClass(int c);//根据c的值在Operand枚举查找指令的类型
+};
+
 #endif
